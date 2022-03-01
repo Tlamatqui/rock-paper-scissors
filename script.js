6aca3e90e00aa291f62eaa4e2c31773f31ee70c4
tlamatqui <tlamatqui@protonmail.com>
2022-03-01 15:29:41 -0800

    create JS pseudocode

---
+++ script.js
@@ -0,0 +1,10 @@
+//rock, paper, scissors game:
+
+//create options
+//initialize scores to 0
+//ask player for choice
+//computer gets random selection
+//compare both selections
+//declare winner
+//increment winner score
+//play another round?
